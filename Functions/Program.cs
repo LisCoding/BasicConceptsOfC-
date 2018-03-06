ae2d857683be85b9a4a14982251829b7fbaaf1ca
Liseth <cardozoliseth@gmail.com>
2018-03-06 09:54:19 -0800

    Update Program.cs
    Add comment to Hobby method

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+        ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +24,7 @@ namespace Functions
             Console.WriteLine(fullName);
         }
 
+        //This is method return a string
         static string Hobby(string favoriteHobby)
         {
             return favoriteHobby;
